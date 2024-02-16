5cac2cf43c23321f369fdb1ec335f656a2666bff
mauripavan <mauriciopavan60@gmail.com>
2024-02-16 13:09:48 -0300

    feat: AddProductModal for feedback

--- src/app/addItem/page.tsx
+++ src/app/addItem/page.tsx
@@ -6,9 +6,13 @@ import { useEffect, useState } from 'react';
 import { useForm, SubmitHandler } from 'react-hook-form';
 import TextInput, { InputType } from '@/components/TextInput';
 import { createProduct } from '@/api';
+import AddProductModal from '@/components/AddProductModal';
 
 export default function AddItem() {
   const [inStock, setInStock] = useState(true);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [addProductError, setAddProductError] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   const AddProductFormSchema = addProductForm();
   type CreateForm = z.infer<typeof AddProductFormSchema>;
@@ -17,6 +21,7 @@ export default function AddItem() {
     handleSubmit,
     watch,
     setValue,
+    reset,
     formState: { errors, isValid },
   } = useForm<CreateForm>({
     mode: 'onTouched',
@@ -24,6 +29,7 @@ export default function AddItem() {
   });
 
   const onSubmit: SubmitHandler<CreateForm> = data => {
+    setLoading(true);
     try {
       const body = {
         description: data.description,
@@ -38,10 +44,17 @@ export default function AddItem() {
         iva: false,
       };
       createProduct({ productData: body }).then(res => {
-        console.log('res :', res);
+        if (res.status === 200) {
+          setModalVisible(true);
+        } else {
+          setAddProductError(true);
+          setModalVisible(true);
+        }
       });
+      setLoading(false);
     } catch (error) {
       console.error(error);
+      setLoading(false);
     }
   };
 
@@ -67,114 +80,130 @@ export default function AddItem() {
     }
   }, [costo, earningPP]);
 
+  const handleCloseModal = () => {
+    setModalVisible(false);
+    addProductError && reset();
+  };
+
   return (
-    <div className='font-nunito'>
-      <h1 className=' font-black'>AGREGAR PRODUCTO</h1>
-      <form className=''>
-        <label className='form-control w-full grid grid-cols-3 gap-8'>
-          <div className='col-span-2'>
-            <TextInput
-              type={InputType.text}
-              placeholder='Descripción'
-              label='Descripción'
-              error={errors.description}
-              registerOptions={{ ...register('description') }}
-            />
-          </div>
-
-          <div className='col-span-1'>
-            <TextInput
-              type={InputType.number}
-              placeholder='Cantidad'
-              label='Cantidad'
-              error={errors.quantity}
-              registerOptions={{ ...register('quantity') }}
-            />
-          </div>
-
-          <div className='col-span-1'>
-            <TextInput
-              type={InputType.number}
-              placeholder='Costo'
-              label='Costo'
-              error={errors.cost}
-              registerOptions={{ ...register('cost') }}
-            />
-          </div>
-
-          <div className='col-span-1'>
-            <TextInput
-              type={InputType.number}
-              placeholder='% Ganacias Isene'
-              label='% Ganacias Isene'
-              error={errors.earningPI}
-              registerOptions={{ ...register('earningPI') }}
-            />
-          </div>
-
-          <div className='col-span-1'>
-            <TextInput
-              type={InputType.number}
-              placeholder='% Ganancias Papeleras'
-              label='% Ganancias Papeleras'
-              error={errors.earningPP}
-              registerOptions={{ ...register('earningPP') }}
-            />
-          </div>
-
-          <div className='col-span-1'>
-            <TextInput
-              type={InputType.number}
-              placeholder='Precio Isene'
-              label='Precio Isene'
-              error={errors.PI}
-              registerOptions={{ ...register('PI') }}
-              disabled
-            />
-          </div>
-
-          <div className='col-span-1'>
-            <TextInput
-              type={InputType.number}
-              placeholder='Precio Papeleras'
-              label='Precio Papeleras'
-              error={errors.PP}
-              registerOptions={{ ...register('PP') }}
-              disabled
-            />
-          </div>
-
-          <div className='col-span-1  font-bold'>
-            <div className=' label '>
-              <span className='label-text'>Stock</span>
+    <>
+      <div className='font-nunito'>
+        <h1 className=' font-black'>AGREGAR PRODUCTO</h1>
+        <form className=''>
+          <label className='form-control w-full grid grid-cols-3 gap-8'>
+            <div className='col-span-2'>
+              <TextInput
+                type={InputType.text}
+                placeholder='Descripción'
+                label='Descripción'
+                error={errors.description}
+                registerOptions={{ ...register('description') }}
+              />
             </div>
-            <select
-              className={`select select-bordered w-full text-center ${
-                inStock ? 'bg-green-200' : 'bg-red-200'
-              }`}
-              {...register('stock')}
-              value={String(inStock)}
-              onChange={handleChange}
-              defaultValue={0}
+
+            <div className='col-span-1'>
+              <TextInput
+                type={InputType.number}
+                placeholder='Cantidad'
+                label='Cantidad'
+                error={errors.quantity}
+                registerOptions={{ ...register('quantity') }}
+              />
+            </div>
+
+            <div className='col-span-1'>
+              <TextInput
+                type={InputType.number}
+                placeholder='Costo'
+                label='Costo'
+                error={errors.cost}
+                registerOptions={{ ...register('cost') }}
+              />
+            </div>
+
+            <div className='col-span-1'>
+              <TextInput
+                type={InputType.number}
+                placeholder='% Ganacias Isene'
+                label='% Ganacias Isene'
+                error={errors.earningPI}
+                registerOptions={{ ...register('earningPI') }}
+              />
+            </div>
+
+            <div className='col-span-1'>
+              <TextInput
+                type={InputType.number}
+                placeholder='% Ganancias Papeleras'
+                label='% Ganancias Papeleras'
+                error={errors.earningPP}
+                registerOptions={{ ...register('earningPP') }}
+              />
+            </div>
+
+            <div className='col-span-1'>
+              <TextInput
+                type={InputType.number}
+                placeholder='Precio Isene'
+                label='Precio Isene'
+                error={errors.PI}
+                registerOptions={{ ...register('PI') }}
+                disabled
+              />
+            </div>
+
+            <div className='col-span-1'>
+              <TextInput
+                type={InputType.number}
+                placeholder='Precio Papeleras'
+                label='Precio Papeleras'
+                error={errors.PP}
+                registerOptions={{ ...register('PP') }}
+                disabled
+              />
+            </div>
+
+            <div className='col-span-1  font-bold'>
+              <div className=' label '>
+                <span className='label-text'>Stock</span>
+              </div>
+              <select
+                className={`select select-bordered w-full text-center ${
+                  inStock ? 'bg-green-200' : 'bg-red-200'
+                }`}
+                {...register('stock')}
+                value={String(inStock)}
+                onChange={handleChange}
+                defaultValue={0}
+              >
+                <option value={'true'}>EN STOCK</option>
+                <option value={'false'}>REPONER</option>
+              </select>
+              {errors.stock && (
+                <p className='text-red-500 font-bold'>
+                  {errors.stock?.message}
+                </p>
+              )}
+            </div>
+          </label>
+          <div className='w-full flex items-center justify-center mt-20 '>
+            <button
+              onClick={handleSubmit(onSubmit)}
+              className='btn btn-xs sm:btn-sm md:btn-md lg:btn-lg bg-blue-400 text-white'
+              disabled={!isValid}
             >
-              <option value={'true'}>EN STOCK</option>
-              <option value={'false'}>REPONER</option>
-            </select>
-            {errors.stock && (
-              <p className='text-red-500 font-bold'>{errors.stock?.message}</p>
-            )}
+              {loading ? (
+                <span className='loading loading-dots loading-md'></span>
+              ) : (
+                <p>AGREGAR PRODUCTO +</p>
+              )}
+            </button>
           </div>
-        </label>
-        <div className='w-full flex items-center justify-center mt-20 '>
-          <button
-            onClick={handleSubmit(onSubmit)}
-            className='btn btn-xs sm:btn-sm md:btn-md lg:btn-lg bg-blue-400 text-white'
-            disabled={!isValid}
-          >
-            <p>AGREGAR PRODUCTO +</p>
-          </button>
-        </div>
-      </form>
-    </div>
+        </form>
+      </div>
+      {modalVisible && (
+        <AddProductModal onClose={handleCloseModal} error={addProductError} />
+      )}
+    </>
   );
 }
