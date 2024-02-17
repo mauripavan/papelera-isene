f8bfd2c4cbc8da26eae5745e3b87fc95e7e0ef28
mauripavan <mauriciopavan60@gmail.com>
2024-02-17 13:56:11 -0300

    feat: add Edit option to ProductCard

--- src/components/TableHeader.tsx
+++ src/components/TableHeader.tsx
@@ -2,7 +2,7 @@ export default function TableHeader() {
   return (
     <div className='grid grid-cols-24 gap-4 w-full px-4 py-2 sticky top-0 items-center mt- bg-gray-10'>
       <p className='font-nunito font-medium text-sm col-span-2'>Codigo</p>
-      <p className='font-nunito font-medium text-sm col-span-9'>Descripcion</p>
+      <p className='font-nunito font-medium text-sm col-span-8'>Descripcion</p>
       <p className='font-nunito font-medium text-sm col-span-2'>Cantidad</p>
       <p className='font-nunito font-medium text-sm col-span-2'>Costo</p>
       <p className='font-nunito font-medium text-sm col-span-2'>PI</p>
