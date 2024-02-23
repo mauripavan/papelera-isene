9fada552abe081fda965f6982c2653563f22d889
mauripavan <mauriciopavan60@gmail.com>
2024-02-23 12:33:15 -0300

    feat: select all

--- src/components/TableHeader.tsx
+++ src/components/TableHeader.tsx
@@ -1,4 +1,23 @@
+'use client';
+
+import { productsState, selectedProductState } from '@/store/app-state';
+import { useState } from 'react';
+import { useRecoilState, useRecoilValue } from 'recoil';
+
 export default function TableHeader() {
+  const [selected, setSelected] = useState(false);
+
+  const [, setSelectedProduct] = useRecoilState(selectedProductState);
+  const products = useRecoilValue(productsState);
+
+  const handleSelectAll = () => {
+    setSelected(prevSelected => !prevSelected);
+    if (selected) {
+      setSelectedProduct([]);
+    } else {
+      setSelectedProduct(products?.data || []);
+    }
+  };
   return (
     <div className='grid grid-cols-24 gap-4 w-full px-4 py-2 sticky top-0 items-center mt- bg-gray-10'>
       <p className='font-nunito font-medium text-sm col-span-2'>Codigo</p>
@@ -11,7 +30,12 @@ export default function TableHeader() {
       <div className='col-span-1'>
         <div className='form-control'>
           <label className='label cursor-pointer'>
-            <input type='checkbox' className='checkbox checkbox-sm' />
+            <input
+              type='checkbox'
+              className='checkbox checkbox-sm'
+              onClick={handleSelectAll}
+              checked={selected}
+            />
           </label>
         </div>
       </div>
