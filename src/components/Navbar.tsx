8b9c79518800543659c5498cf3b14bee4c92e3e1
mauripavan <mauriciopavan60@gmail.com>
2024-04-03 01:10:43 -0300

    fix: Navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import { useRecoilValue } from 'recoil';
+import { useRecoilState } from 'recoil';
 import Logo from './Logo';
 import ProfileIcon from './icons/Profile';
 import { userState } from '@/store/app-state';
@@ -6,41 +6,51 @@ import { logout } from '@/auth';
 import { useRouter } from 'next/navigation';
 
 export default function Navbar() {
-  const user = useRecoilValue(userState);
+  const [user, setUser] = useRecoilState(userState);
   const router = useRouter();
 
   const handleCerrarSesion = () => {
-    logout().then(() => router.replace('/'));
+    logout().then(() => {
+      setUser({
+        username: '',
+        email: '',
+        admin: false,
+        papeleras: false,
+      });
+      router.replace('/');
+    });
   };
 
   return (
     <header className='flex justify-between items-center py-2'>
       <div />
       <Logo />
-      <div className='dropdown'>
-        <div tabIndex={0} role='button' className='btn btn-ghost'>
-          <p className='font-semi-bold'>{`Hola ${user.username}`}</p>
-          <div>
-            <ProfileIcon />
+      {user.username && (
+        <div className='dropdown'>
+          <div tabIndex={0} role='button' className='btn btn-ghost'>
+            <p className='font-semi-bold'>{`Hola ${user.username}`}</p>
+            <div>
+              <ProfileIcon />
+            </div>
           </div>
-        </div>
-        <ul
-          tabIndex={0}
-          className='menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52'
-        >
-          <li onClick={() => router.push('/home')}>
-            <a>Inicio</a>
-          </li>
-          {user.admin && (
-            <li onClick={() => router.push('/dashboard')}>
-              <a>Panel admin</a>
+          <ul
+            tabIndex={0}
+            className='menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52'
+          >
+            <li onClick={() => router.push('/home')}>
+              <a>Inicio</a>
             </li>
-          )}
-          <li onClick={handleCerrarSesion}>
-            <a className='text-red-400'>Cerrar Sesion</a>
-          </li>
-        </ul>
-      </div>
+            {user.admin && (
+              <li onClick={() => router.push('/dashboard')}>
+                <a>Panel admin</a>
+              </li>
+            )}
+            <li onClick={handleCerrarSesion}>
+              <a className='text-red-400'>Cerrar Sesion</a>
+            </li>
+          </ul>
+        </div>
+      )}
     </header>
   );
 }
